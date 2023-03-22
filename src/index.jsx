909b6d55994f72938f3aa083cf870f2f9148cce4
LukeAnger <anger.luke@gmail.com>
2023-03-22 15:49:17 -0400

    added auth components

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = createRoot(document.getElementById("root"))
-
-root.render(<App />)
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-ygryw3g2ezr5ovfp.us.auth0.com"
+    clientId="dBspQB4T8zkhMWgv6RaGd34W0YrlCf2C"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
