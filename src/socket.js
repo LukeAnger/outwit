1789e29e0aabcc7f4e0604c7e33ad901eb85eda0
Joe Lin <joelin3478@gmail.com>
2023-03-22 16:10:14 -0400

    switch from localhost to ec2

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-// export const socket = io("http://ec2-100-26-235-53.compute-1.amazonaws.com:3001/");
+export const socket = io("http://ec2-100-26-235-53.compute-1.amazonaws.com:3001/");
 
-export const socket = io("http://localhost:3001/");
+// export const socket = io("http://localhost:3001/");
