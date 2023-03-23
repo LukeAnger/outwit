a378dd8af363ba69cba68ec03fa2a6a182420e6c
LukeAnger <anger.luke@gmail.com>
2023-03-23 00:04:27 -0400

    styled logout button

--- src/auth/LogoutButton.jsx
+++ src/auth/LogoutButton.jsx
@@ -5,7 +5,9 @@ export const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button
+    id='logoutButton'
+    onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Log Out
     </button>
   );
