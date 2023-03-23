769d917478bdc1da88de79291a22247808dae4a1
LukeAnger <anger.luke@gmail.com>
2023-03-22 23:59:47 -0400

    added auth global variables in utils/config.js

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
-require('dotenv').config();
-
+import { auth } from "./utils/config.js"
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-ygryw3g2ezr5ovfp.us.auth0.com"
-    clientId="dBspQB4T8zkhMWgv6RaGd34W0YrlCf2C"
+    domain={auth.AUTH0_DOMAIN}
+    clientId={auth.AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
