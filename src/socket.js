0d079f6a4a22ad962b0d9aff35ffd91c6931ee81
LukeAnger <anger.luke@gmail.com>
2023-03-28 18:07:34 -0400

    adjust client port again

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 
 
-export const socket = io("http://2607:fb90:18d7:201b:8824:576:b0cc:b271:80/");
+export const socket = io("http://2607:fb90:18d7:201b:8824:576:b0cc:b271/");
